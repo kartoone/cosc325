555499d0b31c2415aa12a28281e2ebb3a101a675
Brian Toone <brtoone@samford.edu>
2023-04-05 13:22:24 -0500

    updated to add support for circle

--- java/ShapeExample.java
+++ java/ShapeExample.java
@@ -7,8 +7,7 @@ public class ShapeExample {
         ArrayList<Shape> shapes = new ArrayList<>(); 
         shapes.add(new Rectangle(0, 0, 25, 5, "#ff0000"));
         shapes.add(new Oval(0, 0, 15, 5, "#ff0000"));
-        shapes.add(new Rectangle(0, 0, 8, 5, "#ff0000"));
-        shapes.add(new Rectangle(0, 0, 3, 4,"#00ff00"));
+        shapes.add(new Circle(0, 0, 3, "#00ff00"));
         shapes.add(new Square(0, 0, 10, "#0000ff"));
         shapes.add(new Triangle(1, 1, 4, 2, 3, 3, "#00ffff"));
         for (Shape s : shapes) {
---
+++ java/shapes/Circle.java
@@ -0,0 +1,10 @@
+package shapes;
+
+public class Circle extends Oval {
+    
+    public Circle(int x, int y, int sidelength, String color) {
+        super(x, y, sidelength, sidelength, color);
+    }
+
+    
+}

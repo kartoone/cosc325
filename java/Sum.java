caddcbf3b3271bf7db3e71ba1e08cbfaed5b38f0
Brian Toone <brtoone@samford.edu>
2026-01-13 09:29:10 -0600

    full java impl

--- java/Sum.java
+++ java/Sum.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class Sum {
+
+    static int sum(int n) {
+        int i;
+        int total = 0;
+        for (i=1; i<=n; i++) {
+            total += i;
+        }
+        return total;
+    }
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = Integer.parseInt(in.nextLine());
+        System.out.printf("%d\n", sum(n));    
+    }
 
 }

e0f786127f5cdc0e2f17cd447144a7b77da1ec80
Brian Toone <brtoone@samford.edu>
2026-02-05 10:01:15 -0600

    more work on lab3 plus some hints for lab4

--- c/lab3/parser.c
+++ c/lab3/parser.c
@@ -1,7 +1,15 @@
 #include "lexer.c"
 
+// data structures needed for the interpreter
+int lineno = 0;   // if this is equal to 0 then we should execute immediately
+char* lines[10];  // preallocate enough room for 10 lines
+int linenos[10];  
+
 void line();
 void statement();
+void expr_list();
+void expression();
+void relop();
 
 /******************************************************/
 /* main driver */
@@ -24,6 +32,9 @@ int main()
 // lex() MUST be called before this function
 void line() {
     if (nextToken == NUMBER) {
+        lineno = atoi(lexeme);
+        // take whatever is left in the rest of the line and store it for processing later!
+
         // consume the token by looking at the line number
         // and storing the statement that follows in the right place in our stored program
         // BUT NOT FOR THIS ASSIGNMENT
@@ -31,23 +42,35 @@ void line() {
         lex();
     }
     statement(); // note that statement MUST have an extra call to lex()
-    if (nextToken != CR) {
+    if (nextToken != CR && nextToken != EOF) {
         printf("Expecting CR, but found: %d instead!\n", nextToken);
     } 
 
 }
 
+// lex() MUST have already been called before here
 void statement() {
     switch(nextToken) {
         case PRINT:
             lex();
             expr_list();
+            // unconditionally printf("\n");
             break;
         case IF:
             lex();
-
-            // extra call to lex to look for the carriage return
+            expression(); 
             lex();
+            relop();
+            lex();
+            expression();
+            lex();
+            if (nextToken != THEN) {
+                printf("error! expecting then found something else");
+            }
+            lex();
+            statement();
+            // we never need an extra call to lex() here 
+            // because statement() ALWAYS has an extra call to lex()
             break;
         case GOTO:
             lex();
@@ -77,13 +100,48 @@ void statement() {
             lex();
             break;
             
-        case RETURN:
-        case CLEAR:
-        case LIST:
-        case RUN:
-        case END:
-            lex(); // this IS the extra call to lex() since nothing comes after these keywords
-
-            break;
+        // case RETURN:
+        // case CLEAR:
+        // case LIST:
+        // case RUN:
+        // case END:
+        //     lex(); // this IS the extra call to lex() since nothing comes after these keywords
+        //     you probably need another call to lex() right here!!!!
+        //     break;
     }
+}
+
+// makes an extra call to lex() to look for the comma
+// lex has ALREADY been called before expr_list
+void expr_list() {
+    if (nextToken == STRING) {
+        // do nothing for this assignment
+        // but in the next assignment you will need to print something!
+    } else {
+        expression();
+    }
+    lex(); // extra call to look for the comma
+    while (nextToken == COMMA) {
+        // next assignment: printf("\t");
+        if (nextToken == STRING) {
+            // do nothing for this assignment
+            // but in the next assignment you will need to print something
+        } else {
+            expression();
+        }
+        lex(); // extra call to look for the comma
+        // there are only two valid tokens AT THIS SPOT
+        if (nextToken != COMMA && nextToken != CR) {
+            printf("Expecting COMMA or CR but found: %d\n", nextToken);
+            exit(1);
+        }
+    }
+}
+
+void expression() {
+    lex(); // you gotta do more than this!
+}
+
+void relop() {
+
 }
